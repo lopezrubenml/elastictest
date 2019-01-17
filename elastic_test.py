4a659d01f28897e60994b0965b299f2db8c52bb3
rlopezlam <ruben.lopez@mercadolibre.com>
2019-01-17 12:22:54 -0300

    Working Post and Get Methods

--- elastic_test.py
+++ elastic_test.py
@@ -1,1 +1,22 @@
 from elasticsearch import Elasticsearch
+
+es = Elasticsearch()
+
+sip = {
+    'name': 'Boris Gromov',
+    'doc': '100109456',
+    'country': 'Russia',
+}
+
+res = es.index(index="test_person", doc_type='sip', id=1, body=sip)
+print(res['result'])
+
+res = es.get(index="test_persons", doc_type='sip', id=1)
+print(res['_source'])
+
+es.indices.refresh(index="test_persons")
+
+res = es.search(index="test_persons", body={"query": {"match_all": {}}})
+print("Got %d Hits:" % res['hits']['total'])
+for hit in res['hits']['hits']:
+    print(hit)
