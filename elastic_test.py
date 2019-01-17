168de812250d6834b0c8ca0c25689c253a08bcb4
rlopezlam <ruben.lopez@mercadolibre.com>
2019-01-17 15:33:48 -0300

    prepare to bulk upload

--- elastic_test.py
+++ elastic_test.py
@@ -1,14 +1,26 @@
 from elasticsearch import Elasticsearch
+import pandas as pd
+from pandas import DataFrame
+import requests
+
 
 es = Elasticsearch()
 
+df = pd.read_csv('C:\\Users\\rlopezlam\\Documents\\2018\\list_search\\base_part_1.csv')
+df_docs = df[~df.doc_number.isna()]
+df_docs = df_docs[['p_name', 'doc_number', 'resident']]
+df_docs.columns = ['name', 'doc', 'country']
+dfj = DataFrame()
+dfj['json'] = df_docs.apply(lambda x: x.to_json(), axis=1)
+sip2 = dfj.iloc[6]['json']
+
 sip = {
-    'name': 'Boris Gromov',
-    'doc': '100109456',
-    'country': 'Russia',
+    'name': 'Tyson Anderson',
+    'doc': 'W894384830',
+    'country': 'Not Known',
 }
 
-res = es.index(index="test_person", doc_type='sip', id=1, body=sip)
+res = es.index(index="test_persons", doc_type='sip', id=181, body=sip2)
 print(res['result'])
 
 res = es.get(index="test_persons", doc_type='sip', id=1)
