27f58a3b31ebd68c585d4964efc4c7bb81057099
rlopezlam <ruben.lopez@mercadolibre.com>
2019-02-11 16:45:17 -0300

    se agrega el form method

--- venv/elasti_app.py
+++ venv/elasti_app.py
@@ -16,6 +16,55 @@ app = Flask(__name__)
 def hello_world():
     return 'Match Tester'
 
+@app.route('/form', methods = ['POST','GET'])
+def form():
+	if request.method == 'POST':
+		name = request.form['name']
+		doc = request.form['doc']
+		country = request.form['country']
+
+		body = {
+			"size": 100,
+			"query": {
+				"bool": {
+					"should": [
+						{"match": {"name": {"query": name, "fuzziness": "2"}}},
+						{"match": {"doc": {"query": doc, "fuzziness": "2"}}}
+					]
+				}
+			}
+		}
+
+		res = es.search(index="test_persons", doc_type="sip", body=body)
+		potential_matches = json_normalize(res['hits']['hits'])
+		if potential_matches.size == 0:
+			return ('No Match1')
+		potential_matches = potential_matches[['_source.country', '_source.name', '_source.doc']]
+		potential_matches.columns = ['list_country', 'list_name', 'list_doc']
+		test_user1 = DataFrame({'user_name': name, 'user_doc': doc, 'user_country': country}, index=[0])
+		name_param1 = 0.7
+		doc_param1 = 0.8
+		site_param1 = 0.2
+		mid_results = match_func_no_counter(test_user=test_user1,
+											list_db=potential_matches,
+											name_param=name_param1,
+											doc_param=doc_param1,
+											site_param=site_param1)
+		name_min = 70
+		doc_min = 70
+		filtered_mid_results = mid_results[(mid_results.name_score > name_min) | (mid_results.doc_score > doc_min)]
+		if filtered_mid_results.size > 0:
+			final_results = potential_matches.loc[list(filtered_mid_results.index)].to_json(orient='index')
+		else:
+			final_results = 'No Match2'
+		return (final_results)
+
+	return '''<form method="POST"> 
+	Name <input type="text" name="name">
+	Doc <input type="text" name="doc">
+	Country <input type="text" name="country">
+	<input type="submit">
+	</form>'''
 
 @app.route('/search/<name>/<doc>/<country>', methods=['GET'])
 def search(name,doc,country):
@@ -25,8 +74,8 @@ def search(name,doc,country):
 	"query":{
 		"bool":{
 			"should":[
-				{"match":{"name":{"query":name, "fuzziness":"1"}}},
-				{"match":{"doc":{"query":doc, "fuzziness":"1"}}}
+				{"match":{"name":{"query":name, "fuzziness":"2"}}},
+				{"match":{"doc":{"query":doc, "fuzziness":"2"}}}
 				]
 			}
 		}
@@ -48,7 +97,7 @@ def search(name,doc,country):
 										name_param=name_param1,
 										doc_param=doc_param1,
 										site_param=site_param1)
-	name_min = 80
+	name_min = 70
 	doc_min = 70
 	filtered_mid_results = mid_results[(mid_results.name_score > name_min) | (mid_results.doc_score > doc_min)]
 	if filtered_mid_results.size > 0:
